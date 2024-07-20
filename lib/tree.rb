c8e6b22d8f8f8fed176ea406103bf2b540451f5e
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-07-20 12:02:01 +0200

    add pretty_print

--- lib/tree.rb
+++ lib/tree.rb
@@ -21,4 +21,11 @@ class Tree
     # Return the root node of the tree
     Node.new(data, left_node, right_node)
   end
+
+    # Pretty print method shared on Discord, made available in the assignment itself
+    def pretty_print(node = @root, prefix = '', is_left = true)
+      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+      puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+     end
 end
