2f849b4a40b8c548b2f04c9f49c24134f60c26fe
Emiel Klijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-08-15 11:18:55 +0200

    Update main.rb
    Completed the project in replit. Here is the final version.

--- main.rb
+++ main.rb
@@ -3,11 +3,45 @@
 require_relative 'lib/node'
 require_relative 'lib/tree'
 
-array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 63, 324]
-test = Tree.new(array)
+# Create a binary search tree from an array of random numbers
+tree = Tree.new((Array.new(15) { rand(1..100) }))
 
-test.pretty_print
-puts "\n\n"
-test.delete(8)
-test.delete(9)
-test.pretty_print
+# Print the tree
+tree.pretty_print
+
+# Confirm that the tree is balanced by calling #balanced?
+puts "Tree balanced? #{tree.balanced?}"
+
+# Print out all elements in level, pre, post, and in order
+print "Level order: "
+p tree.level_order
+print "Pre order: "
+p tree.preorder
+print "Post order: "
+p tree.postorder
+print "In order: "
+p tree.inorder
+
+# Unbalance the tree by adding several numbers > 100
+5.times { tree.insert(rand(101..200)) }
+tree.pretty_print
+
+# Confirm that the tree is unbalanced by calling #balanced?
+puts "Tree balanced? #{tree.balanced?}"
+
+# Balance the tree by calling #rebalance
+tree.rebalance
+tree.pretty_print
+
+# Confirm that the tree is balanced by calling #balanced?
+puts "Tree balanced? #{tree.balanced?}"
+
+# Print out all elements in level, pre, post, and in order.
+print "Level order: "
+p tree.level_order
+print "Pre order: "
+p tree.preorder
+print "Post order: "
+p tree.postorder
+print "In order: "
+p tree.inorder
